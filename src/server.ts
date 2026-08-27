66c58c3210685d7b62bb657d78408da28a4dc191
Shreykhare09 <shreykhare09@gmail.com>
2026-08-27 16:02:17 +0530

    Add PostgreSQL database integration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,25 @@
 import express from "express";
+import pool from "./config/db";
 
 const app = express();
+
+app.get("/db-test", async(req,res)=>{
+    try{
+        const result = await pool.query("SELECT current_database()");
+
+        res.status(200).json({
+            message:"Database connected successfully",
+            databse: result.rows[0].current_database,
+        });
+    }
+    catch(error){
+        console.error("Database connection eerror:", error);
+
+        res.status(500).json({
+            message: "Database connection failed",
+        });
+    }
+});
 
 app.get("/health",(req,res)=>{
     res.status(200).json({
