9877a94dd3a65298b0c5f8687acf79d1d9fdcd21
Shreykhare09 <shreykhare09@gmail.com>
2026-09-18 15:28:37 +0530

    Implement JWT authentication and role authorization

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,7 +1,9 @@
 const secret = process.env.JWT_SECRET;
 
 if (!secret) {
-  throw new Error("JWT_SECRET is not defined");
+    throw new Error("JWT_SECRET is not defined");
 }
+
 const jwtSecret: string = secret;
+
 export { jwtSecret };
